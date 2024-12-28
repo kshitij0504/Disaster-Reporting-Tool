756c5b846aad52fee37b60ee62ce9ac38dae5a8f
KshitijOza <121845519+kshitij0504@users.noreply.github.com>
2024-12-28 16:21:44 +0530

    doNE

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,21 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-// Extend the global type to include prisma as a global property.
-declare global {
-  let prisma: PrismaClient | undefined;
-}
-
-// Create or reuse the Prisma client
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  // In development mode, we use the global prisma instance to avoid creating multiple Prisma clients
-  if (!globalThis.prisma) {
-    globalThis.prisma = new PrismaClient();
-  }
-  prisma = globalThis.prisma;
-}
+// Create a singleton PrismaClient instance
+const prisma = new PrismaClient();
 
 export default prisma;
